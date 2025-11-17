17ff05a0f3293ddd7c5ac238dfc5df917410a7e7
Александр <oksashaok@gmail.com>
2025-11-17 15:24:00 +0300

    0.5 Добавлен метод для вывода 2 матриц.

--- Main.java
+++ Main.java
@@ -5,27 +5,31 @@ public class Main {
     private final static Scanner scanner = new Scanner(System.in);
     private static final byte OFFSET_ZERO = -128;
     private static final byte SIZE = 8;
+    private static final byte MATRICE1 = 0;
+    private static final byte MATRICE2 = 1;
+    private static final String OPTION_90 = "1";
+    private static final String OPTION_180 = "2";
+    private static final String OPTION_270 = "3";
+    private static byte[][][] matrices = new byte[2][SIZE][SIZE];
 
-    private static void conversion(byte array[][]) {
+    private static void conversion() {
         for (byte l0 = 0, l1; l0 < SIZE; ++l0) {
             for (l1 = 0; l1 < SIZE; ++l1) {
                 Random random = new Random();
-                array[l0][l1] = (byte) (OFFSET_ZERO + random.nextInt(256));
+                matrices[0][l0][l1] = (byte) (OFFSET_ZERO + random.nextInt(256));
             }
         }
     }
 
-    private static void turn90DegreeRight(byte array[][]) {
-        byte cache;
+    private static void turn90DegreeRight() {
         final byte MAX = SIZE - 1;
         final byte MIDDLE = SIZE / 2;
         for (byte l0 = 0, l1;;) {// annular displacement
             for (l1 = l0;;) {
-                cache = array[l0][l1];
-                array[l0][l1] = array[MAX - l1][l0];
-                array[MAX - l1][l0] = array[MAX - l0][MAX - l1];
-                array[MAX - l0][MAX - l1] = array[l1][MAX - l0];
-                array[l1][MAX - l0] = cache;
+                matrices[MATRICE2][l1][MAX - l0] = matrices[MATRICE1][l0][l1];
+                matrices[MATRICE2][l0][l1] = matrices[MATRICE1][MAX - l1][l0];
+                matrices[MATRICE2][MAX - l1][l0] = matrices[MATRICE1][MAX - l0][MAX - l1];
+                matrices[MATRICE2][MAX - l0][MAX - l1] = matrices[MATRICE1][l1][MAX - l0];
                 if (MAX - l0 > ++l1) {
                     continue;
                 }
@@ -38,15 +42,13 @@ public class Main {
         }
     }
 
-    private static void turn180DegreeRight(byte array[][]) {
-        byte cache;
+    private static void turn180DegreeRight() {
         final byte MAX = SIZE - 1;
         final byte MIDDLE = SIZE / 2;
         for (byte l0 = 0, l1;;) {// annular displacement
             for (l1 = 0; l1 < SIZE; ++l1) {
-                cache = array[l0][l1];
-                array[l0][l1] = array[MAX - l0][MAX - l1];
-                array[MAX - l0][MAX - l1] = cache;
+                matrices[MATRICE2][MAX - l0][MAX - l1] = matrices[MATRICE1][l0][l1];
+                matrices[MATRICE2][l0][l1] = matrices[MATRICE1][MAX - l0][MAX - l1];
             }
             if (MIDDLE > ++l0) {
                 continue;
@@ -55,17 +57,15 @@ public class Main {
         }
     }
 
-    private static void turn270DegreeRight(byte array[][]) {
-        byte cache;
+    private static void turn270DegreeRight() {
         final byte MAX = SIZE - 1;
         final byte MIDDLE = SIZE / 2;
         for (byte l0 = 0, l1;;) {// annular displacement
             for (l1 = l0;;) {
-                cache = array[l0][l1];
-                array[l0][l1] = array[l1][MAX - l0];
-                array[l1][MAX - l0] = array[MAX - l0][MAX - l1];
-                array[MAX - l0][MAX - l1] = array[MAX - l1][l0];
-                array[MAX - l1][l0] = cache;
+                matrices[MATRICE2][MAX - l1][l0] = matrices[MATRICE1][l0][l1];
+                matrices[MATRICE2][l0][l1] = matrices[MATRICE1][l1][MAX - l0];
+                matrices[MATRICE2][l1][MAX - l0] = matrices[MATRICE1][MAX - l0][MAX - l1];
+                matrices[MATRICE2][MAX - l0][MAX - l1] = matrices[MATRICE1][MAX - l1][l0];
                 if (MAX - l0 > ++l1) {
                     continue;
                 }
@@ -78,22 +78,44 @@ public class Main {
         }
     }
 
-    private static void outputMatrices(byte array[][]) {
-        for (byte l0 = 0, l1; l0 < SIZE; ++l0) {
-            System.out.print("\n");
+    private static void outputMatrices(final String SELECTED_OPTION) {
+        final byte UNSIGNED_CHAR_10 = -118;
+        final byte UNSIGNED_CHAR_100 = -28;
+        for (byte l0 = 0, l1, l2; l0 < matrices.length; ++l0) {
+            if (l0 == 0) {
+                System.out.print("\nДана следующая матрица:\n");
+            } else {
+                switch (SELECTED_OPTION) {
+                    case OPTION_90: {
+                        System.out.print("\n\nРазворот матрицы на 90 градусов в право:\n");
+                    }
+                        break;
+                    case OPTION_180: {
+                        System.out.print("\n\nРазворот матрицы на 180 градусов в право:\n");
+                    }
+                        break;
+                    case OPTION_270: {
+                        System.out.print("\n\nРазворот матрицы на 270 градусов в право:\n");
+                    }
+                        break;
+                }
+            }
             for (l1 = 0; l1 < SIZE; ++l1) {
-                if (array[l0][l1] < -118) {
-                    System.out.print("   " + (array[l0][l1] - OFFSET_ZERO));
-                } else if (array[l0][l1] < -28) {
-                    System.out.print("  " + (array[l0][l1] - OFFSET_ZERO));
-                } else {
-                    System.out.print(" " + (array[l0][l1] - OFFSET_ZERO));
+                System.out.print("\n");
+                for (l2 = 0; l2 < SIZE; ++l2) {
+                    if (matrices[l0][l1][l2] < UNSIGNED_CHAR_10) {
+                        System.out.print("   " + (matrices[l0][l1][l2] - OFFSET_ZERO));
+                    } else if (matrices[l0][l1][l2] < UNSIGNED_CHAR_100) {
+                        System.out.print("  " + (matrices[l0][l1][l2] - OFFSET_ZERO));
+                    } else {
+                        System.out.print(" " + (matrices[l0][l1][l2] - OFFSET_ZERO));
+                    }
                 }
             }
         }
     }
 
-    private static byte menu(byte array[][]) {
+    private static byte menu() {
         System.out.print("""
 
 
@@ -103,22 +125,19 @@ public class Main {
                 3 – 270 градусов.
                 >""");
         switch (scanner.nextLine()) {
-            case "1": {
-                turn90DegreeRight(array);
-                System.out.print("\nРазворот матрицы на 90 градусов в право:\n");
-                outputMatrices(array);
+            case OPTION_90: {
+                turn90DegreeRight();
+                outputMatrices(OPTION_90);
             }
                 return 0;
-            case "2": {
-                turn180DegreeRight(array);
-                System.out.print("\nРазворот матрицы на 180 градусов в право:\n");
-                outputMatrices(array);
+            case OPTION_180: {
+                turn180DegreeRight();
+                outputMatrices(OPTION_180);
             }
                 return 0;
-            case "3": {
-                turn270DegreeRight(array);
-                System.out.print("\nРазворот матрицы на 270 градусов в право:\n");
-                outputMatrices(array);
+            case OPTION_270: {
+                turn270DegreeRight();
+                outputMatrices(OPTION_270);
             }
                 return 0;
             default: {
@@ -129,11 +148,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        byte[][] array = new byte[SIZE][SIZE];
-        conversion(array);
-        System.out.print("\nДана следующая матрица:\n");
-        outputMatrices(array);
-        while (menu(array) != 0)
+        conversion();
+        while (menu() != 0)
             ;
         System.out.print("\n\n");
     }
